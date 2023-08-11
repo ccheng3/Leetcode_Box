efab52ef4b0a82025329f3be43a93c98672f002e
ccheng3 <chrischeng1124@gmail.com>
2023-08-10 18:25:48 -0700

    added the improved soln

--- find_the_town_judge.py
+++ find_the_town_judge.py
@@ -44,3 +44,44 @@ class Solution:
             num_incoming_edges[edge] == n - 1:
                 return edge
         return -1
+    
+
+    # class Solution: ---> Improved solution 
+    # def findJudge(self, n: int, trust: List[List[int]]) -> int:
+    #     # think like a graph 
+    #     # use two dicts 
+    #     # first dict --> for each node in graph, stores number of outgoing edges
+    #     # second dict --> for each node in graph, stores number of incoming edges
+    #     # -> town judge: the node has zero outgoing edges and also has 
+    #     # (N - 1) incoming edges
+    #     #
+    #     # 1) loop thru trust array and count number of outgoing edges for 
+    #     # each node (a_i)
+    #     # 2) loop thru trust array and count number of incmoing edges for 
+    #     # each node (b_i) 
+    #     # 3) loop thru the first dict and find a match in the second dict
+    #     # if exists, else return -1 
+
+    #     # loop thru 'n' and populate the both dictionaries with key 
+    #     # 'n' value and value initialized 0 
+    #     # Runtime: O(V + E) b/c all vertices looked at to populate the dictionaries
+    #     # and all directed edges looked at to count up each vertex's total 
+    #     # num of incoming and outgoing edges. 
+    #     # Space used: O(V) where dictionary sizes are proportional to total num
+    #     # of vertices in the graph. (V key:value pairs in each dict.)
+
+    #     incoming_edges = dict()
+    #     outgoing_edges = dict()
+
+    #     for i in range(1, n + 1):
+    #         incoming_edges[i] = 0
+    #         outgoing_edges[i] = 0
+        
+    #     for relationship in trust:
+    #         outgoing_edges[relationship[0]] += 1
+    #         incoming_edges[relationship[1]] += 1
+        
+    #     for node in incoming_edges.keys():
+    #         if incoming_edges[node] == n - 1 and outgoing_edges[node] == 0:
+    #             return node 
+    #     return -1
